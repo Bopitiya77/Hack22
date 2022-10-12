077b85b9f30876d6f445306bbe10126e65f945ab
Ashu <115586180+ashuqwik@users.noreply.github.com>
2022-10-12 13:29:26 +0530

    Hacktoberfest2022

---
+++ Collections/swap.java
@@ -0,0 +1,14 @@
+public class StudyTonight 
+{ 
+	public static void main(String[] args)   
+	{ 
+		int a=10;
+		int b=20;
+		int temp;
+		System.out.println("Before Swap: a="+a+" b:"+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After Swap: a="+a+" b:"+b);
+	} 
+} 
