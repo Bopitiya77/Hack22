f6813040bae9f5220e7c5c53a9bbbef0ec02931e
CoolCoder3955 <115133249+CoolCoder3955@users.noreply.github.com>
2022-10-17 19:32:42 +0530

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def get_random_string(length):
+    # With combination of lower and upper case
+    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+    # print random string
+    print(result_str)
+
+# string of length 8
+get_random_string(8)
+get_random_string(8)
